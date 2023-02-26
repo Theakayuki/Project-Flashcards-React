b1d5cd40e1140315ecc4444666865db89c71d208
Marcos A Rosas <contactme@marcosarosas.dev>
2023-02-26 02:44:43 -0600

    Study Logic and Component finished

--- src/Layout/Deck/Study/Study.jsx
+++ src/Layout/Deck/Study/Study.jsx
@@ -1,8 +1,150 @@
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
+import { readDeck } from '../../../utils/api';
 
 function Study() {
+    const [deck, setDeck] = useState({ cards: [] });
+    const [currentCard, setCurrentCard] = useState(0);
+    const [isFront, setIsFront] = useState(true);
+    const [isDone, setIsDone] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false);
 
-    const content = <h2>From Study</h2>;
-    return content;
+    const deckId = useParams().deckId;
+
+    // useEffect will load the deck from the api
+    useEffect(() => {
+        const abortController = new AbortController();
+        async function loadDeck() {
+            const loadedDeck = await readDeck(deckId, abortController.signal);
+            setDeck(loadedDeck);
+            setIsLoaded(true);
+        }
+        loadDeck();
+        return () => abortController.abort();
+    }, [deckId]);
+
+    // handleFlip will flip the card from front to back, or back to front
+    const handleFlip = () => {
+        setIsFront(!isFront);
+    };
+
+    // handleNext will move to the next card, if the current card is the last card in the deck, it will set isDone to true
+    const handleNext = () => {
+        if (currentCard < deck.cards.length - 1) {
+            setCurrentCard(currentCard + 1);
+            setIsFront(true);
+        } else {
+            setIsDone(true);
+        }
+    };
+
+    // handleRestart will reset the study session
+    const handleRestart = () => {
+        setCurrentCard(0);
+        setIsFront(true);
+        setIsDone(false);
+    };
+
+
+    // Breadcrumb will display the breadcrumb navigation
+    const Breadcrumb = () => {
+        return (
+            <nav aria-label='breadcrumb'>
+                <ol className='breadcrumb'>
+                    <li className='breadcrumb-item'>
+                        <Link to='/'>
+                            <i className='bi bi-house-door-fill'></i> Home
+                        </Link>
+                    </li>
+                    <li className='breadcrumb-item'>
+                        <Link to={`/decks/${deckId}`}>{deck.name}</Link>
+                    </li>
+                    <li className='breadcrumb-item active' aria-current='page'>
+                        Study
+                    </li>
+                </ol>
+            </nav>
+        );
+    };
+
+    // EndCard will display the end card, which will give the user the option to restart
+    const EndCard = () => {
+        return (
+                    <div className='card'>
+                        <div className='card-body'>
+                            <h2>Done!</h2>
+                            <button className='btn btn-secondary' onClick={handleRestart}>
+                                Restart
+                            </button>
+                        </div>
+                    </div>
+        );
+    };
+
+    // Card will display the current card
+    const Card = () => {
+        return (
+                    <div className='card'>
+                        <div className='card-body'>
+                            <h2>
+                                Card {currentCard + 1} of {deck.cards.length}
+                            </h2>
+                            <p>
+                                {isFront
+                                    ? deck.cards[currentCard].front
+                                    : deck.cards[currentCard].back}
+                            </p>
+                            <button className='btn btn-secondary' onClick={handleFlip}>
+                                Flip
+                            </button>
+                            {isFront ? null : (
+                                <button className='btn btn-primary' onClick={handleNext}>
+                                    Next
+                                </button>
+                            )}
+                        </div>
+                    </div>
+        );
+    };
+
+    // if the deck is not loaded, it will display a loading message
+    if (isLoaded) {
+        if (isDone) {
+
+            // if the deck is done, it will display the end card
+            return (
+                <div>
+                    <Breadcrumb />
+                    <h1>Study: {deck.name}</h1>
+                    <EndCard />
+                </div>
+            );
+        } else {
+
+            // if the deck is not done, it will display the current card
+            return (
+                <div>
+                    <Breadcrumb />
+                    <h1>Study: {deck.name}</h1>
+                    <Card />
+                </div>
+            );
+        }
+    } else {
+
+        // if the deck is not loaded, it will display a loading message
+        return (
+            <div>
+                <h1>Study: {deck.name}</h1>
+                <div className='card'>
+                    <div className='card-body'>
+                        <h2>Loading...</h2>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Study;
